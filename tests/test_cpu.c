b9b7325656bd64bb3f2473b1572e4c9f1480a905
mdx97 <mathewhorner123@gmail.com>
2019-11-23 11:53:38 -0600

    Added more tests for the CPU

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -18,3 +18,22 @@ Test(cpu, move_pc_should_set_the_pc_and_set_the_previous_pc)
     cr_assert(previous_pc == PROGRAM_OFFSET);
     cr_assert(pc == address);
 }
+
+Test(cpu, next_instruction_should_return_the_two_bytes_at_the_pc)
+{
+    initialize_cpu();
+    memory[PROGRAM_OFFSET] = 0xFA;
+    memory[PROGRAM_OFFSET + 1] = 0xBB;
+    uint16_t next = next_instruction();
+    cr_assert(next == 0xFABB);
+}
+
+Test(cpu, previous_instruction_should_return_the_two_bytes_at_the_previous_pc)
+{
+    initialize_cpu();
+    memory[PROGRAM_OFFSET] = 0xFA;
+    memory[PROGRAM_OFFSET + 1] = 0xBB;
+    increment_pc();
+    uint16_t previous = previous_instruction();
+    cr_assert(previous == 0xFABB);
+}
