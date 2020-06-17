43ab30a8f4e008253be2d8242800ef11e0432955
mdx97 <mathewhorner123@gmail.com>
2020-06-16 23:26:57 -0500

    Finished first round of implementation, need unit tests to ensure everything works properly before implementing in the emulator

--- src/set.c
+++ src/set.c
@@ -1,16 +1,50 @@
 #include "set.h"
+#include <stdlib.h>
+
+void initialize(Set *set)
+{
+    set->values = (int *)malloc(INITIAL_SET_SIZE * sizeof(int));
+    set->size = INITIAL_SET_SIZE;
+}
 
 int add(Set *set, int value)
 {
-
+    if (set->count == set->size)
+        double_size(set);
+    set->values[set->count] = value;
+    set->count++;
 }
 
 int remove(Set *set, int value)
 {
-
+    set->values[set->count - 1] = 0;
+    set->count--;
 }
 
 bool contains(Set *set, int value)
 {
-    
+    for (int i = 0; i < set->count; i++) {
+        if (set->values[i] == value)
+            return true;
+    }
+
+    return false;
+}
+
+void destroy(Set *set)
+{
+    free(set->values);
+    set->size = 0;
+    set->count = 0;
+}
+
+void double_size(Set *set)
+{
+    int *new = (int *)malloc(set->size * 2 * sizeof(int));
+    for (int i = 0; i < set->count; i++)
+        new[i] = set->values[i];
+
+    set->size *= 2;
+    free(set->values);
+    set->values = new;
 }
--- src/set.h
+++ src/set.h
@@ -3,12 +3,18 @@
 
 #include <stdbool.h>
 
-typedef struct set_t {
+#define INITIAL_SET_SIZE 4
 
+typedef struct set_t {
+    int *values;
+    int size;
+    int count;
 } Set;
 
+void initialize(Set *set);
 int add(Set *set, int value);
 int remove(Set *set, int value);
 bool contains(Set *set, int value);
+void destroy(Set *set);
 
 #endif
